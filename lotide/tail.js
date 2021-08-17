588630e2f3d37714216f8a322fe49c0fe9d87791
NehaSijaria <nehasijaria@gmail.com>
2021-08-17 01:29:58 -0400

    tail function for assertEqual

---
+++ lotide/tail.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// TEST CODE
+const tail = function(arr){
+  return arr.slice(1);
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
