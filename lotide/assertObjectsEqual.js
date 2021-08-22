5982efeb7bcc55e7f4404aff608470f080cfa956
NehaSijaria <nehasijaria@gmail.com>
2021-08-22 12:55:21 -0400

    Assert Equal in objects

---
+++ lotide/assertObjectsEqual.js
@@ -0,0 +1,31 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+  
+};
+
+const eqObjects = function(object1, object2) {
+  let keyArray1 = Object.keys(object1); // [a, b]
+  if(object1.length != object2.length) {
+    return false;
+  }
+    for (let element of keyArray1) { // Each element of array => a...b...
+      // objects are not equivalent
+      if (object1[element] !== object2[element]) { // value of main object
+          return false; 
+      }
+  }
+  return true;
+};
+
+const ab = { a: '1', b: 2 };
+const ba = { b: 2, a: '1' };
+const ca = { c: 2, a: '1' };
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(ab, ca);
+assertObjectsEqual(ab, abc);
