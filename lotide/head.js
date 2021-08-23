744612d0eca8b7e773339fac6135b3c0e3f2ce80
NehaSijaria <nehasijaria@gmail.com>
2021-08-23 19:51:44 -0400

    refractoring asserEqual and head.js

--- lotide/head.js
+++ lotide/head.js
@@ -1,22 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(1, 14);
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   return arr[0];
 } 
+module.exports = head;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+
