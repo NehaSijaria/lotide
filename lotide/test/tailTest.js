11609b8f8dde56c75020874b32acbff7819d48a4
NehaSijaria <nehasijaria@gmail.com>
2021-08-24 14:48:15 -0400

    mocha implemented

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,10 +1,12 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-//Test codes for tail function
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+describe("#tail", () => {
+  it("should return ['Lighthouse', 'Labs'] from ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']),['Lighthouse', 'Labs']);
+  });
+  it("should return ['Lighthouse', 'Labs'] from ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']),['Hello', 'Labs']);
+  });
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
