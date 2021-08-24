4f7bb8860a4f2d0da8cbcfc824f4f6b476665a92
NehaSijaria <nehasijaria@gmail.com>
2021-08-23 23:19:22 -0400

    Refactoring middle JS

--- lotide/middle.js
+++ lotide/middle.js
@@ -1,15 +1,3 @@
-  const eqArrays = function(arr1, arr2) {
-    if (JSON.stringify(arr1) === JSON.stringify(arr2)) return true;
-  }
-  
-  const assertArraysEqual = function(a1, a2) {
-    if (eqArrays(a1, a2)) {
-      console.log(`✅ Assertion Passed: [${a1}] === [${a1}]`);
-    } else {
-      console.log(`🔴 Assertion Failed: [${a1}] !== [${a2}]`);
-    }
-  }
-  
 const middle = function(arr){
   let newArray = [];
   let len = arr.length;
@@ -26,12 +14,4 @@ const middle = function(arr){
     } 
   }
 
-// TEST CODE
-// ...
-  
-console.log(middle([1, 3, 4])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
+module.exports = middle;
