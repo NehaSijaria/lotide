c21804403054b139d07f850dfd3323e9aabf8595
NehaSijaria <nehasijaria@gmail.com>
2021-08-24 15:38:00 -0400

    Refactoring middle using Mocha and Chai

--- lotide/test/middleTest.js
+++ lotide/test/middleTest.js
@@ -1,7 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 3, 4]), [3]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5], [2, 3])); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe('#middle', () => {
+it('Should returns [3] for [1, 3, 4]', () => {
+assert.deepEqual(middle([1, 3, 4]), [3]);
+});
+it('Should returns [3] for [1, 2, 3, 4, 5]', () => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+});
+it('Should returns [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+});
+
+});
